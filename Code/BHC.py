9e9eb3eaf76b57f16a26335d5000c0eeef8a6827
Xiaodi Qin <xq24@duke.edu>
2017-04-29 19:01:33 -0400

    Finished BHC for multivariate model, added test code for plotting, modified algorithm in the report

--- Code/BHC.py
+++ Code/BHC.py
@@ -1,5 +1,6 @@
-import numpy as np
-from scipy.special import gamma
+from numpy import exp, log
+from functools import partial
+from scipy.special import gamma, gammaln
 
 def bhc(dat, family, alpha, r):
     """Return a matrix in the format of linkage matrix for dendrogram
@@ -9,30 +10,77 @@ def bhc(dat, family, alpha, r):
         @r: scaling factor on the prior precision of the mean
     """
     N, k = dat.shape
-    m = np.mean(dat, axis=0).reshape(k, 1)
-    S = np.cov(dat.T)  # precision?
 
-    # labels
+    if family == "niw":
+        m = np.mean(dat, axis=0).reshape(k, 1)
+        S = np.cov(dat.T)/10 # precision?
+        mlfunc = partial(niw, m=m, S=S, r=r)
 
-    # build tree. Z = [leaf1, leaf2, weight, #leaves]
+    # leaf nodes
+    SS = list(range(N))
+    d0 = [alpha] * N
+    x0 = []
+    ml = []
+    for l in range(N):
+        x0.append((l,))
+        ml.append(mlfunc(dat[l,].reshape(1,k)))
+
+    # paired base cases
+    t = 0; PP = []
+    c1 = []; c2 = []
+    x = []; d = []
+    lp1 = []; lp2 = []; lodds = []
+    for i in range(N-1):
+        for j in range(i+1, N):
+            c1.append(i); c2.append(j)
+            x.append(x0[i]+x0[j])
+            d.append((alpha * gamma(len(x[t])) + d0[i] * d0[j]))    # alpha*alpha
+            lp1.append(mlfunc(dat[x[t],:]) + np.log(alpha) + gammaln(len(x[t])) - np.log(d[t]))
+            lp2.append(ml[i] + ml[j] + np.log(d0[i]) + np.log(d0[j]) - np.log(d[t]))
+            lodds.append(lp1[t] - lp2[t])
+            PP.append(t); t += 1
+
+    # build tree, Z = [leaf1, leaf2, weight, #leaves]
+    p = 0
+    Z = []
+    while(1):
+        idx = lodds.index(max([lodds[y] for y in PP]))
+        Z.append([c1[idx], c2[idx], lodds[idx], len(x[idx])])
+        x0.append(x[idx]); d0.append(d[idx]); ml.append(log(exp(lp1[idx])+exp(lp2[idx])))
+        rm = set(Z[p][:2])
+        SS = [y for y in SS if y not in rm]
+        if len(SS) == 0:
+            break
+
+        for q in SS:
+            c1.append(N+p); c2.append(q)
+            x.append(x0[N+p] + x0[q])
+            d.append(alpha * gamma(len(x[t])) + d0[N+p] * d0[q])
+            lp1.append(mlfunc(dat[x[t],:]) + np.log(alpha) + gammaln(len(x[t])) - np.log(d[t]))
+            lp2.append(ml[N+p] + ml[q] + np.log(d0[N+p]) + np.log(d0[q]) - np.log(d[t]))
+            lodds.append(lp1[t] - lp2[t])
+            PP.append(t); t = t + 1
+
+        PP = [y for y in PP if c1[y] not in rm and c2[y] not in rm]
+        SS.append(N + p); p = p + 1
 
     return Z
 
 
-def scale_matrix(X, m, S):
+def scale_matrix(X, N, k, r, m, S):
     """Return scale matrix for the inverse-Wishart distribution on Sigma.
         @X: N records of data with k columns
         @m: prior on the mean, k * 1
         @S: prior on the covariance, k * k
     """
 
-    xsum = np.sum(X, axis = 0).reshape(d,1) # column sum
+    xsum = np.sum(X, axis = 0).reshape(k,1) # column sum
     t1 = X.T @ X
-    t2 = r * n / (n + r) * (m @ m.T)
-    t3 = 1/(n+r) * (xsum @ xsum.T)
-    t4 = (r / (n + r)) * (m @ xsum.T + xsum @ m.T)
+    t2 = r * N / (N + r) * (m @ m.T)
+    t3 = 1/(N+r) * (xsum @ xsum.T)
+    t4 = (r / (N + r)) * (m @ xsum.T + xsum @ m.T)
 
-    Sprime = S + t1 + t2 + t3 - t4
+    Sprime = S + t1 + t2 - t3 - t4  #? +t3
 
     return Sprime
 
@@ -48,15 +96,15 @@ def niw(X, m, S, r):
     N, k = X.shape
     v = k
     vprime = v + N
-    Sprime = scale_matrix(S)
+    Sprime = scale_matrix(X, N, k, r, m, S)
 
     t1 = (2 * np.pi) ** (- N * k / 2)
     t2 = (r / (N + r)) ** (k/2)
     t3 = np.linalg.det(S) ** (v/2)
     t4 = np.linalg.det(Sprime) ** (-vprime/2)
-    t5num = np.prod(gamma( (vprime + 1 - np.arange(1, d+1))/2 ) ) * (2 ** (vprime * k / 2))
-    t5den = np.prod(gamma( (v + 1 - np.arange(1, d+1))/2 ) ) * (2 ** (v * k / 2))
+    t5num = np.prod(gamma( (vprime - np.arange(k))/2 ) ) * (2 ** (vprime * k / 2))
+    t5den = np.prod(gamma( (v - np.arange(k))/2 ) ) * (2 ** (v * k / 2))
 
-    ml = t1 + t2 + t3 + t4 + (t5num/t5den)
+    ml = t1 * t2 * t3 * t4 * (t5num/t5den)
 
-    return ml
+    return np.log(ml)
--- Code/test.py
+++ Code/test.py
@@ -1,4 +1,3 @@
-import BHC as bh
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram
@@ -10,8 +9,21 @@ dat
 
 # scatter plot
 plt.scatter(dat[:,0], dat[:,1])
+for i in range(dat.shape[0]):
+    plt.annotate(i, (dat[i,0], dat[i,1]))
 plt.show()
 
 # dendrogram (for future use)
-lk = bh.bhc(dat, family = niw, alpha = 1, r = 0.01)
-dendrogram(lk)
+Z = bhc(dat, family = "niw", alpha = 1, r = 0.1)
+Z1 = np.array(Z)
+Z1[:,2] = 1/Z1[:,2]
+maxw = max(Z1[:,2])
+Z1[Z1[:,2] < 0,2] = 2*maxw
+for i in range(Z1.shape[0]):
+    if Z1[i, 0] > (N-1):
+        Z1[i, 2] += Z1[Z1[i, 0].astype("int")-N, 2]
+    if Z1[i,1] > (N-1):
+        Z1[i,2] += Z1[Z1[i,1].astype("int")-N, 2]
+
+dendrogram(Z1)
+plt.show()
