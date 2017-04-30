17840ff3c6662cc0c22c10df87dd3d2c01589118
Xiaodi Qin <xq24@duke.edu>
2017-04-30 02:28:42 -0400

    Added code for color in BHC

--- Code/BHC.py
+++ Code/BHC.py
@@ -2,24 +2,28 @@ from numpy import exp, log
 from functools import partial
 from scipy.special import gamma, gammaln
 
-def bhc(dat, family, alpha, r):
+def bhc(dat, family, alpha, r = 0.001):
     """Return a matrix in the format of linkage matrix for dendrogram
         @dat: N records of data with k columns
-        @family: function to specify prior distribution. Only 'niw' for now
+        @family: function to specify distribution for data. {"multivariate", "bernoulli"}
         @alpha: hyperparameter for the prior
         @r: scaling factor on the prior precision of the mean
     """
     N, k = dat.shape
 
-    if family == "niw":
+    if family == "multivariate":
         m = np.mean(dat, axis=0).reshape(k, 1)
         S = np.cov(dat.T)/10 # precision?
         mlfunc = partial(niw, m=m, S=S, r=r)
+    elif family == "bernoulli":
+        cc=0.01
+        m = np.mean(np.vstack((dat, np.ones(k)*cc, np.zeros(k))), axis=0)
+        alp= m*2; beta=(1-m)*2
+        mlfunc = partial(bb, α=alp, β=beta)
 
     # leaf nodes
     SS = list(range(N))
-    d0 = [alpha] * N
-    x0 = []
+    x0 = []; d0 = [alpha] * N
     ml = []
     for l in range(N):
         x0.append((l,))
@@ -34,18 +38,23 @@ def bhc(dat, family, alpha, r):
         for j in range(i+1, N):
             c1.append(i); c2.append(j)
             x.append(x0[i]+x0[j])
-            d.append((alpha * gamma(len(x[t])) + d0[i] * d0[j]))    # alpha*alpha
+            d.append((alpha * gamma(len(x[t])) + d0[i] * d0[j]))
             lp1.append(mlfunc(dat[x[t],:]) + np.log(alpha) + gammaln(len(x[t])) - np.log(d[t]))
             lp2.append(ml[i] + ml[j] + np.log(d0[i]) + np.log(d0[j]) - np.log(d[t]))
             lodds.append(lp1[t] - lp2[t])
-            PP.append(t); t += 1
-
-    # build tree, Z = [leaf1, leaf2, weight, #leaves]
+            PP.append(t); t = t + 1
+        # build tree, Z = [leaf1, leaf2, weight, #leaves]
     p = 0
     Z = []
+    dye = {}
     while(1):
         idx = lodds.index(max([lodds[y] for y in PP]))
-        Z.append([c1[idx], c2[idx], lodds[idx], len(x[idx])])
+        Z.append([c1[idx], c2[idx], 1/lodds[idx], len(x[idx])])
+        if lodds[idx] < 0:
+            dye[N + p] = "#FF0000"
+        else:
+            dye[N + p] = "#0013FF"
+
         x0.append(x[idx]); d0.append(d[idx]); ml.append(log(exp(lp1[idx])+exp(lp2[idx])))
         rm = set(Z[p][:2])
         SS = [y for y in SS if y not in rm]
@@ -56,6 +65,7 @@ def bhc(dat, family, alpha, r):
             c1.append(N+p); c2.append(q)
             x.append(x0[N+p] + x0[q])
             d.append(alpha * gamma(len(x[t])) + d0[N+p] * d0[q])
+
             lp1.append(mlfunc(dat[x[t],:]) + np.log(alpha) + gammaln(len(x[t])) - np.log(d[t]))
             lp2.append(ml[N+p] + ml[q] + np.log(d0[N+p]) + np.log(d0[q]) - np.log(d[t]))
             lodds.append(lp1[t] - lp2[t])
@@ -64,6 +74,20 @@ def bhc(dat, family, alpha, r):
         PP = [y for y in PP if c1[y] not in rm and c2[y] not in rm]
         SS.append(N + p); p = p + 1
 
+    Z_ = weighted(Z, N)
+
+    return Z_, dye
+
+
+def weighted(Z, N):
+    mw = max([y[2] for y in Z])
+    for i in range(len(Z)):
+        if Z[i][2] < 0:
+            Z[i][2] = 2 * mw
+        if Z[i][0] > (N - 1):
+            Z[i][2] += Z[Z[i][0] - N][2]
+        if Z[i][1] > (N - 1):
+            Z[i][2] += Z[Z[i][1] - N][2]
     return Z
 
 
@@ -108,3 +132,14 @@ def niw(X, m, S, r):
     ml = t1 * t2 * t3 * t4 * (t5num/t5den)
 
     return np.log(ml)
+
+def bb(X, α=0.001, β=0.01):
+    """Return marginal likelihood for bernoulli data using the conjugate prior distribution Bernoulli-Beta
+       @X: N records of data with k columns
+       @α, β: hyperparmeter for Beta distribution
+    """
+    md = np.sum(X,axis=0)
+    N = X.shape[0]
+    num = gammaln(α+β) + gammaln(α+md) + gammaln(β+N-md)
+    den = gammaln(α) + gammaln(β) + gammaln(α+β+N)
+    return np.sum(num - den)
--- Code/test.py
+++ Code/test.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram
 
-# read in data
+# multivariate data
 dat = np.genfromtxt('Data/Simulated/dat1.csv', delimiter=',', skip_header=1)
 dat.shape
 dat
@@ -13,17 +13,19 @@ for i in range(dat.shape[0]):
     plt.annotate(i, (dat[i,0], dat[i,1]))
 plt.show()
 
-# dendrogram (for future use)
-Z = bhc(dat, family = "niw", alpha = 1, r = 0.1)
-Z1 = np.array(Z)
-Z1[:,2] = 1/Z1[:,2]
-maxw = max(Z1[:,2])
-Z1[Z1[:,2] < 0,2] = 2*maxw
-for i in range(Z1.shape[0]):
-    if Z1[i, 0] > (N-1):
-        Z1[i, 2] += Z1[Z1[i, 0].astype("int")-N, 2]
-    if Z1[i,1] > (N-1):
-        Z1[i,2] += Z1[Z1[i,1].astype("int")-N, 2]
+# dendrogram
+Z, color = bhc(dat, family = "multivariate", alpha = 1, r = 0.001)
+dendrogram(Z, link_color_func=lambda k : color[k])
+plt.show()
 
-dendrogram(Z1)
+
+# bernoulli data
+bdat = np.genfromtxt('~/Desktop/bindat.csv', delimiter=',')
+bdat.shape
+X=bdat
+dat = bdat
+
+# dendrogram
+Zb, colorb = bhc(bdat, family = "bernoulli", alpha = 0.001)
+dendrogram(Zb, link_color_func=lambda k : colorb[k])
 plt.show()
